dd12ff9454cca3230d808163746672e2917b599f
202030138SanghyunChoi <shshchoi@naver.com>
2024-10-02 10:30:59 +0900

    Feat: page-project 초기화

---
+++ pages/test/index.jsx
@@ -0,0 +1,7 @@
+export default function Test() {
+  return (
+    <>
+      <h1>Hello, this is Test Page.</h1>
+    </>
+  );
+}
---
+++ pages/test/test2/index.jsx
@@ -0,0 +1,7 @@
+export default function Test2() {
+  return (
+    <>
+      <h1>Hello, this is Second Test Page.</h1>
+    </>
+  );
+}
